4ec295d0e7fe2cbb47c559fffa262293cce4ce97
Mark Downie <mark.downie@nes.scot.nhs.uk>
2021-12-02 09:36:13 +0000

    Added browser link package and added notes

--- NES.Turas.BS4StyleGuide/NES.Turas.BS4StyleGuide/Startup.cs
+++ NES.Turas.BS4StyleGuide/NES.Turas.BS4StyleGuide/Startup.cs
@@ -32,6 +32,7 @@ namespace NES.Turas.BS4StyleGuide
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
